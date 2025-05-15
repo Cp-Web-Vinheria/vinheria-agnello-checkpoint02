8a04fab2d04b2efbebad23af7a68443bf78a39b5
Enzo Brincalepe <enzo.brincalepe@gmail.com>
2025-05-15 16:09:01 -0300

    Adcionando as funções de horario, e de fechamento do site caso menor de idade

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,18 @@
+/*
 // Nome 
-var nome = prompt("digite seu nome:");
+let nome = prompt("digite seu nome:");
 // Caso do nome vazio 
 if (nome == ""){
     alert("Falha, digite algo para continuar");
-    var nome = prompt("digite seu nome:");
+    let nome = prompt("digite seu nome:");
 }
 // idade 
-var anoNascimento = prompt("digite seu ano de nascimento: ");
+let anoNascimento = prompt("digite seu ano de nascimento: ");
 const idade = 2025 - anoNascimento;
 // Caso da idade vazio 
 if(anoNascimento = ""){
     alert("Falha, digite algo para continuar");
-    var anoNascimento = prompt("digite seu ano de nascimento: ");
+    let anoNascimento = prompt("digite seu ano de nascimento: ");
 }
 // Caso menor de idade 
 else if (idade < 18){
@@ -25,29 +26,29 @@ else{
 // detalhes dos vinhos 
 
 // Nome do vinho 
-var nomeVinho = prompt("Qual o nome do seu vinho? ");
+let nomeVinho = prompt("Qual o nome do seu vinho? ");
 
 // Caso do nome do vinho vazio 
 if (nomeVinho == " "){
     alert("Falha, digite o nome de seu vinho ");
-    var nomeVinho = prompt(` - Vinho tinto \n - Vinho Rose \n - Vinho Branco `); 
+    let nomeVinho = prompt(` - Vinho tinto \n - Vinho Rose \n - Vinho Branco `); 
 }
 alert("A seguir, veja os detalhes do vinho no console.");
 console.log(nomeVinho);
 
 // Tipo de vinho
-var tipoVinho = prompt(`Digite o tipo do seu vinho: \n - Vinho tinto \n - Vinho Rose  \n - Vinho Branco `);
+let tipoVinho = prompt(`Digite o tipo do seu vinho: \n - Vinho tinto \n - Vinho Rose  \n - Vinho Branco `);
 
 // Caso do tipo de vinho vazio 
 if (tipoVinho == " "){
     alert("Falha, digite o tipo de seu vinho ");
-    var tipoVinho = prompt(` - Vinho tinto \n - Vinho Rose \n - Vinho Branco `); 
+    let tipoVinho = prompt(` - Vinho tinto \n - Vinho Rose \n - Vinho Branco `); 
 }
 alert("A seguir, veja os detalhes do vinho no console.");
 console.log(tipoVinho);
 
 // Tipo de  safra
-var safraVinho = prompt(`Qual ano da safra do vinho ? `);
+let safraVinho = prompt(`Qual ano da safra do vinho ? `);
 
 if (safraVinho >= 2020){
     alert("Safra de vinho jovem!");
@@ -62,7 +63,7 @@ alert("A seguir, veja os detalhes do vinho no console.");
 console.log(safraVinho);
 
 // Estoque de vinhos
-var estoqueVinho = prompt("Qual a quantidade de vinhos no estoque? ");
+let estoqueVinho = prompt("Qual a quantidade de vinhos no estoque? ");
 
 if (estoqueVinho < 5){
     console.log("ESTOQUE BAIXO");
@@ -72,4 +73,43 @@ console.log(estoqueVinho);
 
 // informaçoes juntas
 console.log(`nome do vinho é ${nomeVinho}, o tipo de vinho é ${tipoVinho}, e a safra de seu vinho é ${safraVinho}, a quantidade é de ${estoqueVinho} vinhos no estoque `);
-   
+   */
+// Educação do site 
+function educacao(){
+    const agora = new Date();
+    const hora = agora.getHours();
+    
+   if (hora >= 5 && hora <12){
+    alert("Bom Dia " + nome)
+   }
+   else if (hora >= 12 && hora < 18){
+    alert("Boa tarde " + nome)
+   }
+   else{
+    alert("Boa Noite " + nome)
+   }
+}
+// Nome 
+var nome = prompt("digite seu nome:");
+// Caso do nome vazio (loop )
+while (nome == ""){
+    alert("Falha, digite algo para continuar");
+    nome = prompt("digite seu nome:");
+}
+educacao()
+// idade 
+let anoNascimento = prompt("digite seu ano de nascimento: ");
+const idade = 2025 - anoNascimento;
+// Caso da idade vazio 
+while(anoNascimento == ""){
+    alert("Falha, digite algo para continuar");
+    anoNascimento = prompt("digite seu ano de nascimento: ");
+}
+// Caso menor de idade 
+if (idade >= 18){
+    alert(`Entrada Autorizada `);
+}
+else{
+    alert(`Você é menor de idade volte em ${18 - idade} anos`)
+    window.close()
+}
